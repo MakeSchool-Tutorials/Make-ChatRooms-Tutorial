59d03aa3b474b184d408108912d7f9b1808230c9
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-18 10:38:48 -0800

    Added pre-populated files

---
+++ Make-ChatRooms-StarterKit/Make-ChatRooms-StarterKit/Helpers/Layouts.swift
@@ -0,0 +1,9 @@
+ //
+//  Layouts.swift
+//  Make-ChatRooms-StarterKit
+//
+//  Created by Matthew Harrilal on 2/18/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
