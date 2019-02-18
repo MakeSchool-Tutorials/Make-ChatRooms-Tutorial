f9c49d7ff2bf8dcab55dd5e9f022562411c48957
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-18 10:30:17 -0800

    Initial commit of the Starter Kit

---
+++ Make-ChatRooms-StarterKit/Make-ChatRooms-StarterKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Make-ChatRooms-StarterKit
+//
+//  Created by Matthew Harrilal on 2/18/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
